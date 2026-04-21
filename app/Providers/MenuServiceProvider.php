67b86a3bdf5af8b5f7cea37ff99dbb3a79babeda
Olivier Gorzalka <olivier@amphibee.fr>
2026-04-21 14:14:30 +0200

    revert: remove Theme\ prefix from namespace placeholder (framework handles it now)

--- app/Providers/MenuServiceProvider.php
+++ app/Providers/MenuServiceProvider.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Theme\%theme_namespace%\Providers;
+namespace %theme_namespace%\Providers;
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
